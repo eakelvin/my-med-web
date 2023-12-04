081e7febe977915cb4f85c13bbfd5fed3f1bacf2
Kelvin Akaba <akabakelvin@gmail.com>
2023-12-04 06:08:09 -0800

    home comp

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import { useLogoutMutation } from '../Slices/userSlice'
 import { clearCredentials } from '../Slices/authSlice'
 import { IoNotificationsCircleOutline } from "react-icons/io5";
 import Dropdown from '../Components/Dropdown'
+import { FaPills } from "react-icons/fa";
 
 const Home = () => {
     const { userInfo } = useSelector((state) => state.auth)
@@ -18,7 +19,7 @@ const Home = () => {
                 <div className="relative flex flex-col w-full min-w-0 mb-6 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30 draggable">
                     <div className="px-9 pt-9 flex-auto min-h-[70px] pb-0 bg-transparent">
                         <div className="flex flex-wrap mb-6 xl:flex-nowrap">
-                            <div className="mb-5 mr-5">
+                            <div className="mr-5">
                                 <div className="relative inline-block shrink-0 rounded-2xl">
                                     <img
                                         className="inline-block shrink-0 rounded-2xl w-[80px] h-[80px] lg:w-[160px] lg:h-[160px]"
@@ -28,9 +29,9 @@ const Home = () => {
                                 </div>
                             </div>
                             <div className="grow">
-                                <div className="flex flex-wrap items-start justify-between mb-2">
+                                <div className="flex flex-wrap items-start justify-between">
                                     <div className="flex flex-col">
-                                        <div className="flex items-center mb-2">
+                                        <div className="flex items-center">
                                             <h3 className="text-secondary-inverse hover:text-blue transition-colors duration-200 ease-in-out font-semibold text-[1.5rem] mr-1">
                                                 {userInfo.firstname} {userInfo.lastname}
                                             </h3>
@@ -59,6 +60,37 @@ const Home = () => {
                 <div>
                     <Link to='/schedule'>View All</Link>
                 </div>
+            </div>
+
+            <div className="p-5 bg-green-500 bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 rounded-lg">
+                <div className="flex">
+                    <div className="border rounded-full w-16 p-3 bg-white">
+                        <FaPills size={40} />
+                    </div>
+                    <div className="ml-3 justify-center">
+                        <p className="text-sm text-white">No of Pills</p>
+                        <p className="text-lg font-bold text-white">Type of Pill</p>
+                        <p className="text-sm text-white text-mute">Time to take</p>
+                    </div>
+                </div>
+                <div className="p-5 text-white">
+                    <p>Date</p>
+                    <p className="">Time</p>
+                </div>
+            </div>
+
+            <div className="mt-10 flex justify-center">
+                <Link to='/reminder'>
+                    <img className='hover:bg-slate-800' src="./addpill.png" alt="" />
+                </Link>
+            </div>
+            <p className='text-center'>Enter medication details and set reminder</p>
+
+
+            <div className="mt-20 flex justify-center">
+                <Link to='/add-medicine' className="text-center rounded-lg w-1/2 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                    ADD MEDICINE
+                </Link>
             </div>
 
         </div>
